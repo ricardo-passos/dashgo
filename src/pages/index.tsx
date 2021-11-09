e9257fdb7be68c240a931bcf33902e352dddf4a6
Ricardo Passos <ricardo.passos.cr@gmail.com>
2021-11-09 18:47:10 -0300

    Buttons now redirect when clicked
    The login button now redirect to the dashboard router.
    The save button at the users/create router now redirect back to the
    users route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
@@ -20,12 +21,13 @@ const signInFormSchema = yup.object().shape({
 })
 
 export default function SignIn() {
+  const router = useRouter()
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(signInFormSchema),
   })
 
   const handleSignin: SubmitHandler<SignInFormData> = (values) => {
-    console.log(values)
+    router.push('/dashboard')
   }
 
   return (
